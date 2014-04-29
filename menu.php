3b19d6d1816445a329cd27b3a606a95a73b019f0
matoboor <matoboor@gmail.com>
2014-04-29 15:35:44 +0200

    initial
    Initial commit

---
+++ menu.php
@@ -0,0 +1,7 @@
+<ul id="menu">
+	<li><a href="../">O nás</a></li>
+	<li><a href="/sluzby/">Naše služby</a></li>
+	<li><a href="#">Rozvoz kvetov</a></li>
+	<li><a href="#">Galéria</a></li>
+	<li><a href="#">Kontakt</a></li>
+</ul>
