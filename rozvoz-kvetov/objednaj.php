b3aae0b2ba6c8332b17da3b010121ab07169cb0f
matoboor <matoboor@gmail.com>
2014-10-16 07:02:52 +0200

    Mail sending OK
    objednaj php get properties from previous form, compose html mail and
    send to seller and customer. then redisrect customer to Ok page. Ok page
    shows timestamp (id of order) obtained form URL parameter.

--- rozvoz-kvetov/objednaj.php
+++ rozvoz-kvetov/objednaj.php
@@ -12,25 +12,99 @@
 	$phone2 = $_POST["phone2"];
 	$address2 = $_POST["address2"];
 	$date = $_POST["date2"];
+	$time = $_POST["time2"];
 	$wish = $_POST["wish"];
 	
 	$customer = "$name1 $surname1";
-	echo "_________Objednavka_________<br />";
-	$message = "customer: $customer \r\n phone: $phone1 \r\n email: $email1 \r\n address: $address1 \r\n\r\n order: $order ";
-	echo $message;
-	
+	$receiver = "$name2 $surname2";
+	date_default_timezone_set('Europe/Bratislava');
+	$timestamp = date('d/m/Y h:i:s');
+	$message = "
+				<html>
+				<head>
+				  <title>Nová objednávka</title>
+				</head>
+				<body>
+				<h2> Objednávka - $timestamp</h2>
+				<hr />
+				  <p>Zákazník</p>
+				  <table>				  
+				    <tr>
+				      <td>Meno</td>
+				      <td>$customer</td>				      
+				    </tr>
+				    <tr>
+				      <td>Telefón</td>
+				      <td>$phone1</td>
+				    </tr>
+				    <tr>
+				      <td>Email</td>
+				      <td><a href=\"mailto:$email1\">$email1</a></td>
+				    </tr>
+				    <tr>
+				      <td>Adresa</td>
+				      <td>$address1</td>
+				    </tr>
+				    <tr>
+				      <td>Objednávka</td>
+				      <td>$order</td>
+				    </tr>
+				  </table>
+				  <hr />
+				  <p>Adresát</p>
+				  <table>
+				  	<tr>
+				      <td>Meno</td>
+				      <td>$receiver</td>
+				    </tr>
+				    <tr>
+				      <td>Telefón</td>
+				      <td>$phone2</td>
+				    </tr>
+				    <tr>
+				      <td>Adresa doručenia</td>
+				      <td>$address2</td>
+				    </tr>
+				    <tr>
+				      <td>Dátum doručenia</td>
+				      <td>$date</td>
+				    </tr>
+				    <tr>
+				      <td>Čas doručenia</td>
+				      <td>$time</td>
+				    </tr>
+				    <tr>
+				      <td>Špecialne prianie</td>
+				      <td>$wish</td>
+				    </tr>
+				  </table>
+				</body>
+				</html>
+				";		
 	$to = "koxo_17@azet.sk";
-	$subject = "New order";
-	$msg = $message;
-	$headers = 'From: matoboor@gmail.com' . "\r\n" .
-    'Reply-To: webmaster@example.com' . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
+	$subject = "Systém prijal novú objednávku";
+	$msg = wordwrap($message,70, "\r\n");
+	$headers  = 'MIME-Version: 1.0' . "\r\n";
+	$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+	$headers .= 'From: Naomi - Kvety <naomi@kvety-naomi.sk>' . "\r\n";
 	if(mail($to,$subject,$msg,$headers))
 	{
-		echo "mail was sended";
+		echo "Objednavka odoslana";
+		if(mail($email1, "Vaša objednávka bola prijatá", $msg, $headers))
+			{
+				echo "Kopia odoslana";
+				header ( "Location: Ok.php?id=$timestamp");
+			}
+			else {
+				echo "chyba";
+			}
 	}
 	else {
-		echo "picu";
-	}
+		echo "chyba";
+		}
+	
+	
+	
+	
 ?>
 </div>
